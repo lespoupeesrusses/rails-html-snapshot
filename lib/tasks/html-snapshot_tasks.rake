9a89bdf14feeab8c9583abc2d4de4459a183be6d
Ethane Tivano <ethane.tivano@mailoo.org>
2012-10-29 12:53:11 +0100

    [inital commit] gem files

---
+++ lib/tasks/html-snapshot_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :html-snapshot do
+#   # Task goes here
+# end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '4fab10ee46e171f0d2d6d27e9623b4516a63f469b659e87ea3802642baa984c83ac2976a1202e9607638bc81eac2ed7289031012ae9550ee21e3a905458dbe93'
